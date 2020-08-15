c3ae07c9f1e44e51385774b3b9c243c0f264de3b
Bice <59291964+certifiedbice@users.noreply.github.com>
2020-08-15 17:27:58 -0600

    Completed basic auth

--- src/components/LoginForm/LoginForm.js
+++ src/components/LoginForm/LoginForm.js
@@ -1,58 +1,37 @@
-import React, { Component } from 'react'
-import { Button, Input } from '../Utils/Utils'
+import React,{Component} from 'react';
+import TokenService from '../../services/token-service';
+import {Button,Input} from '../Utils/Utils';
 
-export default class LoginForm extends Component {
-  static defaultProps = {
-    onLoginSuccess: () => {}
-  }
+export default class LoginForm extends Component{
+	static defaultProps={onLoginSuccess:()=>{}};
+	state={error:null};
 
-  state = { error: null }
+	handleSubmitBasicAuth=ev=>{
+		ev.preventDefault();
+		const {user_name,password}=ev.target;
+		TokenService.saveAuthToken(
+			TokenService.makeBasicAuthToken(user_name.value,password.value)
+		);
+		user_name.value='';
+		password.value='';
+		this.props.onLoginSuccess();
+	}
 
-  handleSubmitBasicAuth = ev => {
-    ev.preventDefault()
-    const { user_name, password } = ev.target
-
-    console.log('login form submitted')
-    console.log({ user_name, password })
-
-    user_name.value = ''
-    password.value = ''
-    this.props.onLoginSuccess()
-  }
-
-  render() {
-    const { error } = this.state
-    return (
-      <form
-        className='LoginForm'
-        onSubmit={this.handleSubmitBasicAuth}
-      >
-        <div role='alert'>
-          {error && <p className='red'>{error}</p>}
-        </div>
-        <div className='user_name'>
-          <label htmlFor='LoginForm__user_name'>
-            User name
-          </label>
-          <Input
-            name='user_name'
-            id='LoginForm__user_name'>
-          </Input>
-        </div>
-        <div className='password'>
-          <label htmlFor='LoginForm__password'>
-            Password
-          </label>
-          <Input
-            name='password'
-            type='password'
-            id='LoginForm__password'>
-          </Input>
-        </div>
-        <Button type='submit'>
-          Login
-        </Button>
-      </form>
-    )
-  }
+	render(){
+		const {error}=this.state;
+		return (
+			<form className='LoginForm' onSubmit={this.handleSubmitBasicAuth}>
+				<div role='alert'>{error && <p className='red'>{error}</p>}</div>
+				<div className='user_name'>
+					<label htmlFor='LoginForm__user_name'>User name</label>
+					<Input name='user_name' id='LoginForm__user_name'></Input>
+				</div>
+				<div className='password'>
+					<label htmlFor='LoginForm__password'>Password</label>
+					<Input name='password' type='password' id='LoginForm__password'></Input>
+				</div>
+				<Button type='submit'>Login</Button>
+			</form>
+		);
+	}
 }
--- src/services/article-api-service.js
+++ src/services/article-api-service.js
@@ -1,56 +1,53 @@
-import config from '../config'
+import TokenService from '../services/token-service';
+import config from '../config';
 
-const ArticleApiService = {
-  getArticles() {
-    return fetch(`${config.API_ENDPOINT}/articles`, {
-      headers: {
-      },
-    })
-      .then(res =>
-        (!res.ok)
-          ? res.json().then(e => Promise.reject(e))
-          : res.json()
-      )
-  },
-  getArticle(articleId) {
-    return fetch(`${config.API_ENDPOINT}/articles/${articleId}`, {
-      headers: {
-      },
-    })
-      .then(res =>
-        (!res.ok)
-          ? res.json().then(e => Promise.reject(e))
-          : res.json()
-      )
-  },
-  getArticleComments(articleId) {
-    return fetch(`${config.API_ENDPOINT}/articles/${articleId}/comments`, {
-      headers: {
-      },
-    })
-      .then(res =>
-        (!res.ok)
-          ? res.json().then(e => Promise.reject(e))
-          : res.json()
-      )
-  },
-  postComment(articleId, text) {
-    return fetch(`${config.API_ENDPOINT}/comments`, {
-      method: 'POST',
-      headers: {
-        'content-type': 'application/json',
-      },
-      body: JSON.stringify({
-        article_id: articleId,
-        text,
-      }),
-    })
-      .then(res =>
-        (!res.ok)
-          ? res.json().then(e => Promise.reject(e))
-          : res.json()
-      )
-  }
+const ArticleApiService={
+	getArticles(){
+		return fetch(`${config.API_ENDPOINT}/articles`,{headers:{}})
+		.then(res=>
+			(!res.ok)
+				? res.json().then(e=>Promise.reject(e))
+				: res.json()
+		);
+	},
+	getArticle(articleId){
+		return fetch(`${config.API_ENDPOINT}/articles/${articleId}`,{
+			headers:{'authorization':`basic ${TokenService.getAuthToken()}`}
+		})
+		.then(res=>
+			(!res.ok)
+				? res.json().then(e=>Promise.reject(e))
+				: res.json()
+		);
+	},
+	getArticleComments(articleId){
+		return fetch(`${config.API_ENDPOINT}/articles/${articleId}/comments`,{
+			headers:{'authorization':`basic ${TokenService.getAuthToken()}`}
+		})
+		.then(res=>
+			(!res.ok)
+				? res.json().then(e=>Promise.reject(e))
+				: res.json()
+		);
+	},
+	postComment(articleId,text){
+		return fetch(`${config.API_ENDPOINT}/comments`,{
+			method:'POST',
+			headers:{
+				'content-type':'application/json',
+				'authorization':`basic ${TokenService.getAuthToken()}`
+			},
+			body:JSON.stringify({
+				article_id:articleId,
+				text,
+			}),
+		})
+		.then(res=>
+			(!res.ok)
+				? res.json().then(e=>Promise.reject(e))
+				: res.json()
+		);
+	}
 }
 
 export default ArticleApiService
